460df576f9c2416c11bfc81fcd7ad88a23770ceb
avishek7713 <avishek0424@gmail.com>
2025-01-06 00:05:51 -0500

    Printing list of items with price over 6

--- parsing_data/parse_xml.py
+++ parsing_data/parse_xml.py
@@ -5,7 +5,13 @@ file_path = "groceries.xml"
 tree = ET.parse(file_path)
 root = tree.getroot()
 
+items_over_6 = []
+
 for item in root.findall("grocery_item"):
     name = item.find("name").text
     price = item.find("price").text
+    if float(price) > 6.00:
+        items_over_6.append(name)
     print(name, price)
+
+print("Items with price over 6: ", items_over_6)
